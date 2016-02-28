ee2e7663706449ff1562de0ad845ff9099f4d526
David Conner <dconner.pro@gmail.com>
2016-02-28 00:10:30 -0500

    init commit.  muse lib is working with iOS. no bitcode problems.  adding metal

---
+++ SpectralMuseOSXBridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  SpectralMuseOSXBridgingHeader.h
+//  SpectralMuseOSX
+//
+//  Created by David Conner on 2/27/16.
+//  Copyright © 2016 Spectra. All rights reserved.
+//
+
+#ifndef SpectralMuseOSXBridgingHeader_h
+#define SpectralMuseOSXBridgingHeader_h
+
+#import "Muse.h"
+
+#endif /* SpectralMuseOSXBridgingHeader_h */
